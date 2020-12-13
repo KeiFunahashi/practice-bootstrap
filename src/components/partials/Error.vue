08286434230559ae2b8b32b1c206f768a01a8134
船橋渓 <funahashikei@team-lab.com>
2020-12-13 23:11:18 +0900

    template作成

---
+++ src/components/partials/Error.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.error
+  .tirle {{title}}
+  .message {{message}}
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Error extends Vue {
+  @Prop({ type: String, required: true })
+  public title!: string
+
+  @Prop({ type: String, required: true })
+  public message!: string
+}
+</script>
---
+++ src/layouts/error.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.errorWrap
+  ErrorPage(
+    :title="errorTitle"
+    :message="errorMessage"
+  )
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import ErrorPage from '@/components/partials/Error.vue'
+
+@Component({
+  components: {
+    ErrorPage,
+  },
+})
+export default class Error extends Vue {
+  @Prop({ type: Object, required: true })
+  public error: any
+
+  public errorTitle!: string
+  public errorMessage!: string
+
+  created() {
+    this.errorTitle = this.error.statusCode
+    this.errorMessage = this.error.message
+  }
+}
+</script>
