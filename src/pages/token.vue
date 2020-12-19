50553c1c6dfab34e9525f9a855df7ab09ca3561a
船橋渓 <funahashikei@team-lab.com>
2020-12-19 11:27:11 +0900

    トークン設定ページを作成

---
+++ src/pages/token.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.token
+  h1 トークン設定
+  .container token
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Default extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
