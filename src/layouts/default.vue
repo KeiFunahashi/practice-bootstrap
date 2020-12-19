0300cc62f21bf20a5ef8a2536ff7d4b85b98a1b2
船橋渓 <funahashikei@team-lab.com>
2020-12-19 11:25:46 +0900

    レイアウトを修正

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,23 @@
-<template>
-  <div>
-    <Nuxt />
-  </div>
+<template lang="pug">
+.app
+    Header
+    .main
+     Nuxt
 </template>
 
-<style>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import Header from '@/components/partials/Header/header.vue'
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class Default extends Vue {}
+</script>
+
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -16,40 +29,14 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
+body {
+  margin: 0;
+}
 
 *,
 *::before,
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
