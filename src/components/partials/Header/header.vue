a67fb33950d46f49abd731620928c532b28ea899
船橋渓 <funahashikei@team-lab.com>
2020-12-19 11:25:33 +0900

    ヘッダーコンポーネントを作成

---
+++ src/components/partials/Header/header.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.header
+  .appName
+    nuxt-link(to="/")
+      h1 商品登録
+  .content
+    nuxt-link(to="/")
+      span 商品一覧
+    nuxt-link(to="new")
+      span 新規作成
+    nuxt-link(to="search")
+      span 商品検索
+    nuxt-link(to="token")
+      span トークン認証
+  .user ユーザー情報
+</template>
+<script lang="ts"></script>
+<style>
+.appName a {
+  text-decoration: none;
+}
+.header {
+  background-color: rgba(167, 167, 167, 0.16);
+  height: 96px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 60px;
+}
+.content {
+  text-align: center;
+}
+span {
+  padding: 0 20px;
+}
+</style>
