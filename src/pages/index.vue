d74ede41a1b53edb304280e346f62e387e6d3cad
船橋渓 <funahashikei@team-lab.com>
2020-12-19 21:26:01 +0900

    モックデータで商品一覧を作成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,27 @@
 <template lang="pug">
 .index
   h1 商品一覧
-  .container index
+    .container(v-for="product in products")
+      img.productImage(:src="product.image")
+      .productName {{product.name}}
+      .productPrice ￥{{product.price}}-
+      .productDescription {{product.description}}
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import { ProductState } from '@/store/types'
 
 @Component({})
-export default class Default extends Vue {}
+export default class Default extends Vue {
+  public products: ProductState = this.$store.state.products
+}
 </script>
 
 <style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
