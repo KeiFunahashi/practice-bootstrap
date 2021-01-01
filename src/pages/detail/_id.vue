5a4ed7bb547de189a62326579f0ca08927264f5e
船橋渓 <funahashikei@team-lab.com>
2021-01-01 20:09:19 +0900

    商品詳細ページを作成

---
+++ src/pages/detail/_id.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.search
+  h1 商品詳細
+  .container
+    .productDetail
+      img.productImage(:src="product.image")
+      .productName {{product.name}}
+      .productPrice ￥{{product.price}}-
+      .productDescription {{product.description}}
+</template>
+
+<script lang="ts">
+import { ProductState } from '@/store/types'
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Default extends Vue {
+  product: ProductState = this.$store.getters['Product/productDetail'](
+    Number(this.$route.params.id)
+  )
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+}
+</style>
