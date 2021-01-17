9d93192d7093c1d4daf10da5f841936ffce72198
船橋渓 <funahashikei@team-lab.com>
2021-01-17 17:25:37 +0900

    apiのURLを定数化

---
+++ src/common/constants/api.ts
@@ -0,0 +1,7 @@
+/** API のエンドポイント */
+export const ENDPOINT = {
+  // エンドポイント
+  API_ENDPOINT: 'http://localhost:8080/',
+  // Productの関係URL
+  PRODUCTS_API_URL: 'http://localhost:8080/api/products/',
+}
